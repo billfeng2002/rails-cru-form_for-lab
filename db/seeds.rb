716a1736f76421f499bfb739170451752f48ce8d
Bill <billfeng2002@gmail.com>
2021-07-06 14:14:06 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Genre.destroy_all
+Artist.destroy_all
+Song.destroy_all
+
+10.times do |t|
+    Genre.create(name:"pop#{t}")
+end
+
+10.times do |t|
+    Artist.create(name: "artist#{t}")
+end
+
+20.times do |t|
+    Song.create(name: "song#{t}", artist_id: Artist.ids.sample, genre_id: Genre.ids.sample)
+end
